63bbf5309383aa0e91fb0b9b456de48ad869133d
bashyerOmar <besho.om.alghamdi@gmail.com>
2022-06-03 00:29:57 +0300

    like assignment

---
+++ JS Course/JS/likes.js
@@ -0,0 +1,21 @@
+
+function like(){
+    var num = document.getElementById("likes-num");
+    var likecounter = parseInt(num.innerHTML)+1;
+    num.innerHTML = likecounter;
+
+}
+
+function like2(){
+    var num = document.getElementById("likes-num2");
+    var likecounter = parseInt(num.innerHTML)+1;
+    num.innerHTML = likecounter;
+
+}
+
+function like3(){
+    var num = document.getElementById("likes-num3");
+    var likecounter = parseInt(num.innerHTML)+1;
+    num.innerHTML = likecounter;
+
+}
