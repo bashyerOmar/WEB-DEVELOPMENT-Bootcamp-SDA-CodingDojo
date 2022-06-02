a3daff761d7d88b30cb675c0c0a3edb0a149c0c3
bashyerOmar <besho.om.alghamdi@gmail.com>
2022-06-03 00:29:37 +0300

    button clicker assignment

---
+++ JS Course/JS/button_clicker.js
@@ -0,0 +1,16 @@
+function loginBtn(e) {
+    // when we click the button 
+    //change button text from login to logout 
+    e.innerText = "Log out";
+}
+
+
+// remove the button when we click it
+function removeDef(e){
+    e.remove();
+}
+
+// when click the button alert message will appear
+function alert(){
+    alert('ninja was liked');
+}
