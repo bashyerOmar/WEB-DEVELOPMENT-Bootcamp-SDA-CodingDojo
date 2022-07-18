5d5b72e62cab8b1052b6f2394f72163228884f08
bashyerOmar <besho.om.alghamdi@gmail.com>
2022-07-18 14:44:56 +0300

    try add guest's order

--- java/Java Fundamentals/OrdersItems/TestOrders.java
+++ java/Java Fundamentals/OrdersItems/TestOrders.java
@@ -14,19 +14,28 @@ public class TestOrders {
         // Order instances 
         Order order1=new Order("Cindhuri");
         order1.addItem(item3);//Add item3 to order1's items list
+        order1.addItem(item2);
         order1.setReady(); //Cindhuri’s order is now ready. Update her status
 
         Order order2=new Order("Jimmy");
         order2.addItem(item1); //Add item1 to order2's item list
+        order2.addItem(item3);
         order2.setReady();
 
         Order order3=new Order("Noah");
         order3.addItem(item4); //order3 is a cappucino
-    
-        Order order4=new Order("Sam");
-        order4.addItem(item2); // first order
-        order4.addItem(item2); //second order (2 latte)
+        order3.addItem(item2);
+
+        // guest's odrers
+        Order order4=new Order();
         order4.addItem(item2);
+        order4.addItem(item1);
+        order4.setReady();
+        Order order5=new Order();
+        order5.addItem(item4);
+        order5.addItem(item3);
+        
+        
      
        
 
@@ -35,6 +44,7 @@ public class TestOrders {
       order2.display();
       order3.display();
       order4.display();
+      order5.display();
 
 
         
