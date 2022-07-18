3a7930c97cf02f0faf8a6fde4809359baf7b50b0
bashyerOmar <besho.om.alghamdi@gmail.com>
2022-07-18 14:35:35 +0300

    add getters and setters methods and constructor

--- java/Java Fundamentals/OrdersItems/Item.java
+++ java/Java Fundamentals/OrdersItems/Item.java
@@ -1,8 +1,32 @@
 package OrdersItems;
 
 public class Item {
-  String name;
-  double price;
+  private String name;
+  private double price;
+
+  // CONSTRUCTOR
+  public Item(String name , double price){
+    this.name =name;
+    this.price=price;
+
+  }
+
+  //getters and setters 
+  public void setName(String name){
+     this.name=name;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public void setPrice(double price){
+      this.price =price;
+  }
+
+  public double getPrice (){
+    return this.price;
+  }
 
   
 }
--- java/Java Fundamentals/OrdersItems/Order.java
+++ java/Java Fundamentals/OrdersItems/Order.java
@@ -2,8 +2,71 @@ package OrdersItems;
 import java.util.ArrayList;
 
 public class Order {
-    String name;
-    double total;
-    boolean ready;
-    ArrayList<Item> items = new ArrayList<Item>();
+    private String name;
+    private double total;
+    private boolean ready;
+    private ArrayList<Item> items ;
+
+    // CONSTRUCTOR
+    // No arguments, sets name to "Guest", initializes items as an empty list.
+    public Order(){
+      this.name="Guest";
+      this.items= new ArrayList<Item>();
+    }
+    
+    // OVERLOADED CONSTRUCTOR
+    // Takes a name as an argument, sets name to this custom name.
+    // Initializes items as an empty list.
+    public Order (String name){
+      this.name=name;
+      this.items= new ArrayList<Item>();
+    }
+
+    
+    public void setName (String name){
+          this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public double getOrderTotal(){
+        for (int i=0; i< this.items.size(); i++){ // sum all the item's prices to get the total 
+            this.total += this.items.get(i).getPrice();
+         }
+        return this.total;
+    }
+
+    public void setReady(){
+        this.ready =true;
+    }
+
+    public boolean isReady(){
+        return this.ready;
+    }
+
+    public String getStatusMessage(){
+        if (isReady()){
+            return "Your order is ready";
+        }
+        // if the order not ready
+        return "Thank you for waiting, Your order will be ready soon.";
+    }
+
+    public void addItem(Item item){
+        this.items.add(item);
+    }
+
+    public ArrayList<Item> getItems(){
+        return this.items;
+    }
+
+    public void display(){
+        System.out.printf("Customer Name: %s\n", this.getName());
+        for (Item item : this.getItems()){
+            System.out.println(item.getName()+" - "+ item.getPrice());
+         }
+         System.out.println("Total :"+this.getOrderTotal() +" \n\n");
+    }
 }
